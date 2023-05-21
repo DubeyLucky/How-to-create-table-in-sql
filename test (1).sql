cdc1ce440eeef6183b4335217ab1db27a595e4c5
DubeyLucky <114846353+DubeyLucky@users.noreply.github.com>
2023-05-21 18:55:14 +0530

    Create test (1).sql

---
+++ test (1).sql
@@ -0,0 +1,26 @@
+
+
+create DATABASE Lucky; /* created DATABASE */
+show Databases;      /*saw databse */
+use Lucky;         /*use database */
+create table student(
+    id int,
+    name VARCHAR(100),
+    email VARCHAR(200)); /*create table */
+show TABLEs;    
+desc student;
+
+/* entering data in table */
+
+/*insert into student(id,name,email) VALUES (1,"Lucky","Luckykumar@gmail.com") */
+
+select * from student;
+
+desc student;
+
+
+
+
+
+
+
